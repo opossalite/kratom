f8f7301d5dee3dacf573b0a363f880ccd879323b
opossalite <werbird10@gmail.com>
2024-06-01 09:52:00 +0000

    Bumped version to 1.0 and added basic test

--- kratom/src/lib.rs
+++ kratom/src/lib.rs
@@ -15,6 +15,14 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn test_creation_and_forward() {
+        let mut neuralnet = neuralnets::default((4, 200, 300, 20));
+        let inputs = vec![1.0, 2.0, 4.2, -1.0];
+        let output = neuralnet.forward(&inputs);
+        assert_eq!(20, output.len());
+    }
 }
 
 
