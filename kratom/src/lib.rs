a5f414dd5e3c835749f4264b95bb005dee2d7570
opossalite <werbird10@gmail.com>
2024-06-01 10:40:41 +0000

    Removed default contents

--- kratom/src/lib.rs
+++ kratom/src/lib.rs
@@ -2,19 +2,11 @@ pub mod tools;
 pub mod neuralnets;
 pub mod multipliers;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn test_creation_and_forward() {
